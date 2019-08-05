925805898c88a17dfdab288de77cddc70366bf79
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-08-05 17:24:55 -0300

    Implementação do Sol e do Plutão

---
+++ SistemaSolar/Assets/Scripts/RotacionaObjeto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotacionaObjeto : MonoBehaviour
+{
+    [SerializeField] float velocidade;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * velocidade * Time.deltaTime);
+    }
+}
